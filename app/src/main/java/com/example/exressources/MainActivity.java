13cedef3a56e9a07fb63e86aa8389d5a02a7184a
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-04 14:03:08 +0000

    Premier exemple de chaine

--- app/src/main/java/com/example/exressources/MainActivity.java
+++ app/src/main/java/com/example/exressources/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.exressources;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t = findViewById(R.id.t);
+
+        String ch2 = getString(R.string.chaine2);
+
+        t.setText(ch2);
     }
 }
